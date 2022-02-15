b4ab4f1b70aa52c3f4a83744ea1ce513a01d784a
Rachel Ly <rachel.ly17@gmail.com>
2022-02-14 19:18:57 -0800

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Rachel Ly';
+var isCool = true;
+var totalPets = 2;
+var unknown = null;
+var never;
+
+console.log('Value of fullName: ' + fullName);
+console.log(typeof fullName);
+
+console.log('Value of isCool: ' + isCool);
+console.log(typeof isCool);
+
+console.log('Value of totalPets: ' + totalPets);
+console.log(typeof totalPets);
+
+console.log('Value of unknown: ' + unknown);
+console.log(typeof unknown);
+
+console.log('Value of never: ' + never);
+console.log(typeof never);
