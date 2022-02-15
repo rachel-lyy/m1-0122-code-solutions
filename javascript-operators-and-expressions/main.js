b4370eb3c2b583593f62a120b2555a08e8cfd88f
Rachel Ly <rachel.ly17@gmail.com>
2022-02-14 21:06:48 -0800

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 100;
+var height = 150;
+var area = width * height;
+console.log('Value of area: ' + area);
+console.log('typeof area: ' + typeof area);
+
+var bill = 32;
+var payment = 36;
+var change = payment - bill;
+console.log('Value of change: ' + change);
+console.log('typeof change: ' + typeof change);
+
+var quizzes = 59;
+var midterm = 98;
+var final = 87;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Value of average: ' + grade);
+console.log('typeof grade: ' + typeof grade);
+
+var firstName = 'Rachel';
+var lastName = 'Ly';
+var fullName = firstName + ' ' + lastName;
+console.log('Value of fullName: ' + fullName);
+console.log('typeof fullName: ' + typeof fullName);
+
+var pH = 9;
+var isAcidic = pH > 7;
+console.log('Value of isAcidic: ' + isAcidic);
+console.log('typeof isAcidic: ' + typeof isAcidic);
+
+var headCount = 49;
+var isSparta = (headCount === 300);
+console.log('Value of isSparta: ' + isSparta);
+console.log('typeof isSparta: ' + typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('Value of motto: ' + motto);
+console.log('typeof motto: ' + typeof motto);
