6b418c03c57b13c3810d556eba98749ea1934691
rachel <rachel@seedcms.com>
2022-03-07 18:53:53 -0800

    Completed dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,14 @@
+console.log('hello, world');
+
+console.log('The H1: ', document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
+
+console.log('The explanation id value: ', document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+
+console.log('The hint class value:', document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
+
+console.log('Log all p element: ', document.querySelectorAll('p'));
+
+console.log('Find all elements with the class of "example-link": ', document.querySelectorAll('.example-link'));
